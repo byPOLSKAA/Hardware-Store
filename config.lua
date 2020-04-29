a8c7667c9ebc52207047c12da8eb536b5b36d0b0
byPOLSKAA <64549677+byPOLSKAA@users.noreply.github.com>
2020-04-29 19:43:16 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config              = {}
+Config.DrawDistance = 100
+Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
+Config.Color        = {r = 0, g = 128, b = 255}
+Config.Type         = 1
+Config.Locale = 'en'
+
+Config.Zones = {
+
+	Hardware = {
+		Items = {},
+		Pos = {
+			{x = 2747.91,   y = 3472.8,  z = 55.67},
+			{x = 548.49,   y = -172.84,  z = 54.48}
+		}
+	}
+}
---
+++ esx_hardware.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `hardware` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`store` varchar(100) NOT NULL,
+	`item` varchar(100) NOT NULL,
+	`price` int(11) NOT NULL,
+
+	PRIMARY KEY (`id`)
+);
